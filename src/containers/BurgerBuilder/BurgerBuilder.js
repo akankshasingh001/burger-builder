0b067aef2842000069c9c703312e88b687b9d9e8
Akanksha Singh <singhakanksha001@gmail.com>
2020-03-29 12:27:37 -0400

    displayed price of burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -73,6 +73,7 @@ class BurgerBuilder extends Component {
             ingredientAdded={this.addIngredientsHandler}
             ingredientRemoved={this.removeIngredientsHandler}
             disabled={disabledInfo}
+            price={this.state.totalPrice}
           />
         </div>
       </Fragment>
