408e95db1bb51eab20abc5f3e5d32ec9c4ab5d55
Akanksha Singh <singhakanksha001@gmail.com>
2020-03-29 12:21:33 -0400

    logic added for remove ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,6 +19,8 @@ class BurgerBuilder extends Component {
     },
     totalPrice: 4
   };
+
+  //add ingredient(s) to burger
   addIngredientsHandler = type => {
     const oldCount = this.state.ingredients[type];
     const updatedCount = oldCount + 1;
@@ -34,14 +36,44 @@ class BurgerBuilder extends Component {
       ingredients: updatedIngredients
     });
   };
+
+  //remove Ingredient(s) from burger
+  removeIngredientsHandler = type => {
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) {
+      return;
+    }
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({
+      totalPrice: newPrice,
+      ingredients: updatedIngredients
+    });
+  };
   render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
     return (
       <Fragment>
         <div>
           <Burger ingredients={this.state.ingredients} />
         </div>
         <div>
-          <BuildControls ingredientAdded={this.addIngredientsHandler} />
+          <BuildControls
+            ingredientAdded={this.addIngredientsHandler}
+            ingredientRemoved={this.removeIngredientsHandler}
+            disabled={disabledInfo}
+          />
         </div>
       </Fragment>
     );
