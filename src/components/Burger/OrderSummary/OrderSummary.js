b6d28fcbd89bf102277793f8866c70f6f134ddbf
Akanksha Singh <singhakanksha001@gmail.com>
2020-03-29 16:37:07 -0400

    Added custom button

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -15,6 +15,8 @@ const orderSummary = props => {
       <p>A delicius burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to checkout?</p>
+      <button>CANCEL</button>
+      <button>CONTINUE</button>
     </Fragment>
   );
 };
